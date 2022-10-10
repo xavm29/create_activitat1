ff787e041baee78c66e522bbd6414f633257030f
xavier <xavm29@gmail.com>
2022-10-10 12:58:45 +0200

    CREACIÓ DE L’APPLICACIÓ FLUTTER INICIAL importacio llibreria Generacio noms aleatoris Importacio package Error Creacio fitxer people_list.dart sense errors Modificar el archiu main.dart y people_list.dart CREEU UN LISTVIEW DEDESPLAÇAMENT INFINIT pagina 12

--- lib/people_list.dart
+++ lib/people_list.dart
@@ -11,6 +11,8 @@ class PeopleList extends StatefulWidget {
 class _PeopleListState extends State<PeopleList> {
   @override
   Widget build(BuildContext context) {
+    final _suggestions = <String>[];                 // NEW
+    final _biggerFont = const TextStyle(fontSize: 18); // NEW
     final randomNames = RandomNames(Zone.us); // Nou
     return Text(randomNames.fullName()); // Nou
   }
