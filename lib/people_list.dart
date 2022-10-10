ab6d8d780f182d35249e258e364ab0f04dd716db
xavier <xavm29@gmail.com>
2022-10-10 12:45:01 +0200

    CREACIÓ DE L’APPLICACIÓ FLUTTER INICIAL importacio llibreria Generacio noms aleatoris Importacio package Error Creacio fitxer people_list.dart sense errors

---
+++ lib/people_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PeopleList extends StatefulWidget {
+  const PeopleList({Key? key}) : super(key: key);
+
+  @override
+  State<PeopleList> createState() => _PeopleListState();
+}
+
+class _PeopleListState extends State<PeopleList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
